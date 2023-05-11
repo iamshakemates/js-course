b558a88ab59b34bb7ce7a9e0795d3dbd3796e507
iamshakemates <johnpatrickbruzovalencia@gmail.com>
2023-05-11 21:17:29 +0800

    added save button

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let count = 0;
 let countEL = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
 
 function incCount() {
   count = count + 1;
   countEL.innerText = count;
+  //   console.log(count);
+}
+
+function saveBtn() {
   console.log(count);
 }
