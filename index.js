a8690de586aa89f73705c316ba5c8a97714bb9d2
iamshakemates <johnpatrickbruzovalencia@gmail.com>
2023-05-11 22:01:11 +0800

    added save button and render it on the page

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let count = 0;
 let countEL = document.getElementById("count-el");
 let saveEL = document.getElementById("save-el");
+let nameEL = document.getElementById("name-el");
 
 function incCount() {
   count = count + 1;
   countEL.innerText = count;
-  //   console.log(count);
 }
 
 function saveBtn() {
-  console.log(count);
+  let nameStr = count + "-";
+  nameEL.innerText += nameStr;
 }
