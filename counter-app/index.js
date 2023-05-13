682b98d45ef6abe741cc4f6bd4f71f8b7cfd5b05
iamshakemates <johnpatrickbruzovalencia@gmail.com>
2023-05-13 16:03:26 +0800

    Move the counter app into folder

--- counter-app/index.js
+++ counter-app/index.js
@@ -1,17 +1,67 @@
-let countEL = document.getElementById("count-el");
-let nameEL = document.getElementById("name-el");
-let count = 0;
+// VARIABLE DECLARATION
+let fisrtCard = getRandomNumber();
+let secondCard = getRandomNumber();
 
-// INCREMENT FUNCTION
-function incCount() {
-  count = count + 1;
-  countEL.textContent = count;
+let sumEL = document.getElementById("sum-el");
+let messageEL = document.getElementById("message-el");
+let cardsEL = document.getElementById("cards-el");
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
+let nameEL = document.getElementById("name-el");
+
+let sum = fisrtCard + secondCard;
+let cards = fisrtCard + " " + secondCard;
+let cardo = [fisrtCard, secondCard];
+
+let player = {
+  name: "Jaypee",
+  chips: 150,
+};
+
+// FUNCTION DECLARATION
+nameEL.textContent = player.name + ": $" + player.chips;
+
+function getRandomNumber() {
+  let randomNum = Math.floor(Math.random() * 12) + 1;
+  if (randomNum > 10) {
+    return 10;
+  } else if (randomNum === 1) {
+    return 11;
+  } else {
+    return randomNum;
+  }
 }
 
-// SAVE FUNCTION
-function saveBtn() {
-  let nameStr = count + " - ";
-  nameEL.textContent += nameStr;
-  countEL.textContent = 0;
-  count = 0;
+function startBtn() {
+  renderFn();
+}
+
+function renderFn() {
+  sumEL.textContent = "Sum: " + sum;
+  cardsEL.textContent = "Cards: ";
+
+  for (let x = 0; x < cardo.length; x++) {
+    cardsEL.textContent += cardo[x] + " ";
+  }
+
+  if (sum < 21) {
+    message = "Draw new card?";
+  } else if (sum === 21) {
+    message = "You've got blackjack";
+    hasBlackjack = true;
+  } else {
+    message = "You're out of the game";
+    isAlive = false;
+  }
+  messageEL.textContent = message;
+}
+
+function newBtn() {
+  if (isAlive === true && hasBlackjack === false) {
+    let newCard = getRandomNumber();
+    cardo.push(newCard);
+    sum += newCard;
+    renderFn();
+  }
 }
