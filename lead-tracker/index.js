79297be8746b7e4761cebad5f86fd775e8ef25bf
iamshakemates <johnpatrickbruzovalencia@gmail.com>
2023-05-15 11:29:40 +0800

    Interacting with DOM and local storage

--- lead-tracker/index.js
+++ lead-tracker/index.js
@@ -3,10 +3,27 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const saveEl = document.getElementById("save-el");
 const ulEl = document.getElementById("ul-el");
+const deleteEL = document.getElementById("delete-el");
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 saveEl.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+  console.log(localStorage.getItem("myLeads"));
+});
+
+deleteEL.addEventListener("dblclick", function () {
+  console.log("dblclicked");
+  localStorage.clear();
+  myLeads = [];
   renderLeads();
 });
 
@@ -16,11 +33,6 @@ function renderLeads() {
     listItems += `<li>
       <a target= '_blank' href = '${myLeads[i]}'>${myLeads[i]}</a>
       </li>`;
-    // below is for reference:
-    // -------------------------------------
-    // let li = document.createElement("li");
-    // li.textContent = inputArr[i];
-    // ulEl.append(li);
   }
   ulEl.innerHTML = listItems;
 }
