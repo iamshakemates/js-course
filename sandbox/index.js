dfa69c2fd969a902a6c90e4d33ae105a1309cd2c
iamshakemates <johnpatrickbruzovalencia@gmail.com>
2023-05-16 17:01:13 +0800

    Loops

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,14 +1,40 @@
-const person = {
-  name: "Shake",
-  age: 20,
-  hobbies: ["basketball", "coding", "music"],
-  address: {
-    street: "Macabulos",
-    city: "Makati",
-  },
-};
+// const todos = [
+//   {
+//     id: 1,
+//     task: "Throw the trash",
+//     isDone: true,
+//   },
+//   {
+//     id: 2,
+//     task: "Code for 30 min",
+//     isDone: false,
+//   },
+//   {
+//     id: 3,
+//     task: "Order food",
+//     isDone: false,
+//   },
+// ];
 
-// destructuring object to create variables
-const { name, age } = person;
+// const todoJSON = JSON.stringify(todos);
 
-console.log(name);
+// console.log(todoJSON);
+
+const shoes = ["nike", "jordan", "reebok"];
+
+// while loop
+let i = 0;
+while (i < 3) {
+  console.log(shoes[i]);
+  i++;
+}
+
+// for loop
+for (let i = 0; i < 3; i++) {
+  console.log(shoes[i]);
+}
+
+//  for-of loop
+for (let shoe of shoes) {
+  console.log(shoe);
+}
