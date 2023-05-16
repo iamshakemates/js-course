1b2cd5707c2ef1e69e184642d459ce07e14b5025
iamshakemates <johnpatrickbruzovalencia@gmail.com>
2023-05-16 20:57:33 +0800

    Classes

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,14 +1,17 @@
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dob = new Date(dob);
-}
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+  }
 
-Person.prototype.getYear = function () {
-  return this.dob.getFullYear();
-};
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
 
 const person1 = new Person("John", "Doe", "06-19-1995");
 
-console.log(person1.getYear());
-GIT;
+console.log(person1);
+
+console.log(person1.getFullName());
